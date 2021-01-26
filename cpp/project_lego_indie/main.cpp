8743c729baa66cd77d3eeec75705e89e1dd9a7ad
Tobias Egger <s1910567016@fhooe.at>
2021-01-26 18:55:49 +0100

    Refactored Code; Didnt make it more beautiful, but more readable.

--- cpp/project_lego_indie/main.cpp
+++ cpp/project_lego_indie/main.cpp
@@ -13,6 +13,7 @@
 #include <memory>
 #include <iostream>
 #include <filesystem>
+#include <optional>
 
 // opencv
 #include <opencv2/core.hpp>
@@ -48,64 +49,80 @@ cv::Mat imreadChecked(const std::filesystem::path& f, cv::ImreadModes m){
     return img;
 }
 
-int main(int argc, char** argv)
-{
-
-    // wm icon
-    std::shared_ptr<Fl_RGB_Image> icon = std::make_shared<Fl_RGB_Image>(icon_data, 128, 128, ImgShow::fl_imgtype::rgba, 0);
-
+std::optional<po::variables_map> parseCmdLine(int argc, char** argv){
     // commandline options
     po::options_description desc("Allowed options");
     desc.add_options()
-    ("help", "Print help.")
-    ("background", po::value<std::string>(), "Background image. (defaults to ./pic/Other/image_100.jpg)")
-    ("templdir", po::value<std::string>(), "Folder containing template files. (defaults to ./pic/templates)")
-    ("image", po::value<std::string>(), "Image to be used. (if not set or invalid a gui prompt will force you to select one)");
+            ("help", "Print help.")
+            ("background", po::value<std::string>(), "Background image. (defaults to ./pic/Other/image_100.jpg)")
+            ("templdir", po::value<std::string>(), "Folder containing template files. (defaults to ./pic/templates)")
+            ("image", po::value<std::string>(), "Image to be used. (if not set or invalid a gui prompt will force you to select one)");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
-    // print help message
     if (vm.count("help")) {
         std::cout << desc << std::endl;
-        return EXIT_SUCCESS;
+        return {};
     }
+    return vm;
+}
 
+struct r_val {
+    std::filesystem::path bg_img_path;
+    std::filesystem::path path;
+    std::filesystem::path templDir;
+};
+
+r_val getFromCmdLine(po::variables_map vm){
     std::filesystem::path bg_img_path = "./pic/Other/image_100.jpg";
+    std::filesystem::path path = "";
+    std::filesystem::path templDir = "./pic/templates";
+
     if(vm.count("background")){
         bg_img_path = vm["background"].as<std::string>();
     }
-
-    std::filesystem::path path = "";
     if(vm.count("image")){
         path = vm["image"].as<std::string>();
     }
-
     if(!std::filesystem::exists(path)){
         path = fl_file_chooser("Choose image file...", "*.jpg", "./pic/All/image_0.jpg", 1);
     }
-
-    std::filesystem::path templDir = "./pic/templates";
     if(vm.count("templdir")){
         templDir = vm["templdir"].as<std::string>();
     }
 
+    return {bg_img_path, path, templDir};
+}
+
+int main(int argc, char** argv)
+{
+
+    // wm icon
+    auto icon = std::make_shared<Fl_RGB_Image>(icon_data, 128, 128, ImgShow::fl_imgtype::rgba, 0);
+    auto vm_b = parseCmdLine(argc, argv);
+    if(vm_b == std::nullopt){
+        return EXIT_SUCCESS;
+    }
+    auto vm = vm_b.value();
+    auto paths = getFromCmdLine(vm);
+
     // read images
-    cv::Mat img = imreadChecked(path, cv::IMREAD_COLOR);
-    cv::Mat bg_img = imreadChecked(bg_img_path, cv::IMREAD_COLOR);
-    cv::Mat templIndie = imreadChecked(templDir.append("template.png"), cv::IMREAD_COLOR);
-    cv::Mat templBody = imreadChecked(templDir.remove_filename().append("mask_body.png"), cv::IMREAD_COLOR) & templIndie;
-    cv::Mat templFace = imreadChecked(templDir.remove_filename().append("mask_face.png"), cv::IMREAD_COLOR) & templIndie;
-    cv::Mat templHat = imreadChecked(templDir.remove_filename().append("mask_hat.png"), cv::IMREAD_COLOR) & templIndie;
-    cv::Mat templHead = imreadChecked(templDir.remove_filename().append("mask_head.png"), cv::IMREAD_COLOR) & templIndie;
-    cv::Mat templLarm = imreadChecked(templDir.remove_filename().append("mask_larm.png"), cv::IMREAD_COLOR) & templIndie;
-    cv::Mat templRarm = imreadChecked(templDir.remove_filename().append("mask_rarm.png"), cv::IMREAD_COLOR) & templIndie;
-    cv::Mat templLhand = imreadChecked(templDir.remove_filename().append("mask_lhand.png"), cv::IMREAD_COLOR) & templIndie;
-    cv::Mat templRhand = imreadChecked(templDir.remove_filename().append("mask_rhand.png"), cv::IMREAD_COLOR) & templIndie;
-    cv::Mat templLleg = imreadChecked(templDir.remove_filename().append("mask_lleg.png"), cv::IMREAD_COLOR) & templIndie;
-    cv::Mat templRleg = imreadChecked(templDir.remove_filename().append("mask_rleg.png"), cv::IMREAD_COLOR) & templIndie;
-    templDir.remove_filename();
+    cv::Mat img = imreadChecked(paths.path, cv::IMREAD_COLOR);
+    cv::Mat bg_img = imreadChecked(paths.bg_img_path, cv::IMREAD_COLOR);
+    cv::Mat templIndie = imreadChecked(paths.templDir.append("template.png"), cv::IMREAD_COLOR);
+    cv::Mat templBody = imreadChecked(paths.templDir.remove_filename().append("mask_body.png"), cv::IMREAD_COLOR) & templIndie;
+    cv::Mat templFace = imreadChecked(paths.templDir.remove_filename().append("mask_face.png"), cv::IMREAD_COLOR) & templIndie;
+    cv::Mat templHat = imreadChecked(paths.templDir.remove_filename().append("mask_hat.png"), cv::IMREAD_COLOR) & templIndie;
+    cv::Mat templHead = imreadChecked(paths.templDir.remove_filename().append("mask_head.png"), cv::IMREAD_COLOR) & templIndie;
+    cv::Mat templLarm = imreadChecked(paths.templDir.remove_filename().append("mask_larm.png"), cv::IMREAD_COLOR) & templIndie;
+    cv::Mat templRarm = imreadChecked(paths.templDir.remove_filename().append("mask_rarm.png"), cv::IMREAD_COLOR) & templIndie;
+    cv::Mat templLhand = imreadChecked(paths.templDir.remove_filename().append("mask_lhand.png"), cv::IMREAD_COLOR) & templIndie;
+    cv::Mat templRhand = imreadChecked(paths.templDir.remove_filename().append("mask_rhand.png"), cv::IMREAD_COLOR) & templIndie;
+    cv::Mat templLleg = imreadChecked(paths.templDir.remove_filename().append("mask_lleg.png"), cv::IMREAD_COLOR) & templIndie;
+    cv::Mat templRleg = imreadChecked(paths.templDir.remove_filename().append("mask_rleg.png"), cv::IMREAD_COLOR) & templIndie;
+    paths.templDir.remove_filename();
 #ifdef _WIN32
 	FreeConsole();
 #endif
